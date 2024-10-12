8f59dc7af9bb059b040213ecc3a5983c90c542e3
swami1302 <swamii1413@gmail.com>
2024-10-12 19:26:45 +0530

    shopcontext updated

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -7,7 +7,29 @@ const ShopContextProvider=(props)=>{
     const currency='$';
     const delivery_fee=10;
     const [search, setsearch] = useState("");
-    const [showSearch, setshowSearch] = useState(true);
+    const [showSearch, setshowSearch] = useState(false);
+    const [cartItems, setcartItems] = useState({});
+
+    const addToCart= async(itemId,size)=>{
+        let cartData=structuredClone(cartItems);
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size]++
+            }else{
+                cartData[itemId][size]=1;
+            }
+        }else{
+            cartData[itemId]={};
+            cartData[itemId][size]=1;
+        }
+        setcartItems(cartData);
+    }
+
+    useEffect(()=>{
+        console.log(cartItems);
+        
+    },[cartItems]);
+
     const value={
         products,delivery_fee,currency,search,showSearch,setsearch,setshowSearch,cartItems,setcartItems,addToCart
     }
