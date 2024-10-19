84e2d6cf1b4666ca3d6cbcda7a723695e3b4aab6
swami1302 <swamii1413@gmail.com>
2024-10-20 00:03:43 +0530

    Login.jsx updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 function Navbar() {
   const [visible, setvisible] = useState(false);
-  const {setshowSearch,getCartCount} = useContext(ShopContext);
+  const { setshowSearch, getCartCount } = useContext(ShopContext);
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to="/">
@@ -42,13 +42,19 @@ function Navbar() {
         </NavLink>
       </ul>
       <div className="flex items-center gap-6">
-        <img onClick={()=>setshowSearch(true)} src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        <img
+          onClick={() => setshowSearch(true)}
+          src={assets.search_icon}
+          className="w-5 cursor-pointer"
+          alt=""
+        />
         <div className="group relative">
-          <img
+          <Link to='/login'> <img
             src={assets.profile_icon}
             className="w-5 cursor-pointer"
             alt=""
           />
+          </Link>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profile</p>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+  const [currentState, setcurrentState] = useState('Sign Up');
+
+  const onSubmitHandler = async (e)=>{
+    e.preventDefault();
+    console.log("Form submit");
+    
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={(e)=>onSubmitHandler(e)} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800 '>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p className='prata-regular text-3xl'>{currentState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800 '/>
+      </div>
+      {currentState==='Login'? '' : <input type="text" className='w-full px-3 py-2 border border-gray-800 '  placeholder='name' required/>}
+      <input type="email" className='w-full px-3 py-2 border border-gray-800 '  placeholder='email' required/>
+      <input type="password" className='w-full px-3 py-2 border border-gray-800 '  placeholder='password' required/>
+      <div className='w-full flex justify-between text-sm mt-[-8px]'>
+        <p className='cursor-pointer '>Forgot your password?</p>
+        {
+          currentState==='Login'
+          ? <p className='cursor-pointer capitalize' onClick={()=>setcurrentState('Sign Up')}>Create Account</p>
+          : <p className='cursor-pointer capitalize' onClick={()=>setcurrentState('Login')}>Login here</p>
+
+        }
+      </div>
+      <button className='bg-black text-white font-light px-8 py-2 mt-4 capitalize'>{currentState==='Login' ? 'Sign in' : 'Sign up'}</button>
+    </form>
   )
 }
 
